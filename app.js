4645f21dffbb39774cfdc2b423b0eba1e1212764
larchenkoroman <larchenkoroman@gmail.com>
2024-02-15 20:11:06 +0200

    CheckPaddleCollisions

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
 
-const BALL_SIZE = 5;
+const BALL_SIZE = 7;
 const ballPosition = {x:20, y:30};
 let xSpeed = 4;
 let ySpeed = 2;
 
-const PADDLE_WIDTH = 5;
-const PADDLE_HEIGHT = 20;
+const PADDLE_WIDTH = 10;
+const PADDLE_HEIGHT = 40;
 const PADDLE_OFFSET = 10;
 
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
 
 document.addEventListener('mousemove', e => {
-    rightPaddleTop = e.y < PADDLE_HEIGHT
-                     ? 0
-                     : e.y > height - PADDLE_HEIGHT
+    rightPaddleTop = e.y > height - PADDLE_HEIGHT
                      ? height- PADDLE_HEIGHT
                      : e.y;
 });
@@ -57,12 +55,57 @@ function update() {
     ballPosition.y += ySpeed;
 }
 
+function checkPaddleCollision(ball, paddle) {
+    return ball.left < paddle.right
+        && ball.right > paddle.left
+        && ball.top < paddle.bottom
+        && ball.bottom > paddle.top;
+}
+
+function adjustAngle(distanceFromTop, distanceFromBottom) {
+    if(distanceFromTop < 0) {
+        // If ball hit near top of paddle, reduce ySpeed
+        ySpeed -= 0.5;
+    } else if (distanceFromBottom < 0) {
+        // If ball hit near bottom of paddle, increase ySpeed
+        ySpeed += 0.5;
+    }
+}
+
 function checkCollision() {
     let ball = {
         left: ballPosition.x,
         right: ballPosition.x + BALL_SIZE,
         top: ballPosition.y,
         bottom: ballPosition.y + BALL_SIZE
+    }
+    let leftPaddle = {
+        left: PADDLE_OFFSET,
+        right: PADDLE_OFFSET + PADDLE_WIDTH,
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT
+    };
+    let rightPaddle = {
+        left: width - PADDLE_WIDTH - PADDLE_OFFSET,
+        right: width - PADDLE_OFFSET,
+        top: rightPaddleTop,
+        bottom: rightPaddleTop + PADDLE_HEIGHT
+    };
+
+    if(checkPaddleCollision(ball, leftPaddle)) {
+        // Left paddle collision happened
+        let distanceFromTop = ball.top - leftPaddle.top;
+        let distanceFromBottom = leftPaddle.bottom - ball.bottom;
+        adjustAngle(distanceFromTop, distanceFromBottom);
+        xSpeed = Math.abs(xSpeed);
+    }
+
+    if(checkPaddleCollision(ball, rightPaddle)) {
+        // Right paddle collision happened
+        let distanceFromTop = ball.top - rightPaddle.top;
+        let distanceFromBottom = rightPaddle.bottom - ball.bottom;
+        adjustAngle(distanceFromTop, distanceFromBottom);
+        xSpeed = -Math.abs(xSpeed);
     }
 
     if(ball.left < 0 || ball.right > width) {
