ab28dab7124b43a910385502989ab1f551fcae28
larchenkoroman <larchenkoroman@gmail.com>
2024-02-15 16:55:50 +0200

    MouseMove

--- app.js
+++ app.js
@@ -9,13 +9,47 @@ const ballPosition = {x:20, y:30};
 let xSpeed = 4;
 let ySpeed = 2;
 
+const PADDLE_WIDTH = 5;
+const PADDLE_HEIGHT = 20;
+const PADDLE_OFFSET = 10;
+
+let leftPaddleTop = 10;
+let rightPaddleTop = 30;
+
+document.addEventListener('mousemove', e => {
+    rightPaddleTop = e.y < PADDLE_HEIGHT
+                     ? 0
+                     : e.y > height - PADDLE_HEIGHT
+                     ? height- PADDLE_HEIGHT
+                     : e.y;
+});
+
+
 
 function draw() {
+    //Заливаем холст черным
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, width, height);
 
+    //всё остальное будет белым
     ctx.fillStyle = 'white';
+
+    //рисуем "шарик"
     ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
+
+    //рисуем ракетки
+    ctx.fillRect(
+        PADDLE_OFFSET,
+        leftPaddleTop,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT
+    )
+    ctx.fillRect(
+        width - PADDLE_WIDTH - PADDLE_OFFSET,
+        rightPaddleTop,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT
+    )
 }
 
 function update() {
