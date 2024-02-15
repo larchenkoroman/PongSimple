accf006a351c3584dbc837d3e22646ff04550001
larchenkoroman <larchenkoroman@gmail.com>
2024-02-15 11:28:46 +0200

    CheckCollisions

--- app.js
+++ app.js
@@ -23,9 +23,26 @@ function update() {
     ballPosition.y += ySpeed;
 }
 
+function checkCollision() {
+    let ball = {
+        left: ballPosition.x,
+        right: ballPosition.x + BALL_SIZE,
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE
+    }
+
+    if(ball.left < 0 || ball.right > width) {
+        xSpeed = -xSpeed;
+    }
+    if(ball.top < 0 || ball.bottom > height) {
+        ySpeed = - ySpeed;
+    }
+}
+
 function gameLoop() {
     draw();
     update();
+    checkCollision();
 
     setTimeout(gameLoop, 30);
 }
