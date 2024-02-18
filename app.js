d7ddc205a6521f1ba7bbc7507e4f5ff913265e33
larchenkoroman <larchenkoroman@gmail.com>
2024-02-18 17:27:34 +0200

    FollowBall

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
 
+const MAX_COMPUTER_SPEED = 2;
 const BALL_SIZE = 7;
-const ballPosition = {x:20, y:30};
-let xSpeed = 4;
-let ySpeed = 2;
+// const ballPosition = {x:20, y:30};
+let ballPosition;
+let xSpeed;
+let ySpeed;
 
 const PADDLE_WIDTH = 10;
 const PADDLE_HEIGHT = 40;
@@ -15,6 +17,8 @@ const PADDLE_OFFSET = 10;
 
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
+let leftScore = 0;
+let rightScore = 0;
 
 document.addEventListener('mousemove', e => {
     rightPaddleTop = e.y > height - PADDLE_HEIGHT
@@ -22,6 +26,27 @@ document.addEventListener('mousemove', e => {
                      : e.y;
 });
 
+function initBall() {
+    ballPosition = {x:20, y:30};
+    xSpeed = 4;
+    ySpeed = 2;
+}
+
+function followBall() {
+    let ball = {
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE
+    };
+    let leftPaddle = {
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT
+    };
+    if(ball.top < leftPaddle.top) {
+        leftPaddleTop -= MAX_COMPUTER_SPEED;
+    } else if(ball.bottom > leftPaddle.bottom) {
+        leftPaddleTop += MAX_COMPUTER_SPEED;
+    }
+}
 
 
 function draw() {
@@ -48,11 +73,19 @@ function draw() {
         PADDLE_WIDTH,
         PADDLE_HEIGHT
     )
+
+    // Draw scores
+    ctx.font = "30px monospace";
+    ctx.textAlign = "left";
+    ctx.fillText(leftScore.toString(), 50, 50);
+    ctx.textAlign = "right";
+    ctx.fillText(rightScore.toString(), width - 50, 50);
 }
 
 function update() {
     ballPosition.x += xSpeed;
     ballPosition.y += ySpeed;
+    followBall();
 }
 
 function checkPaddleCollision(ball, paddle) {
@@ -108,9 +141,16 @@ function checkCollision() {
         xSpeed = -Math.abs(xSpeed);
     }
 
-    if(ball.left < 0 || ball.right > width) {
-        xSpeed = -xSpeed;
+    if(ball.left < 0) {
+        rightScore++;
+        initBall();
     }
+
+    if(ball.right > width) {
+        leftScore++;
+        initBall();
+    }
+
     if(ball.top < 0 || ball.bottom > height) {
         ySpeed = - ySpeed;
     }
@@ -124,4 +164,5 @@ function gameLoop() {
     setTimeout(gameLoop, 30);
 }
 
+initBall();
 gameLoop();
